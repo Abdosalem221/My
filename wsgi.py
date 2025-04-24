fda1aa5eda3b8e502c1764cf7bb3f1b99dfa3f54
Abdosalem221 <SAMYHOSSAM080@GMAIL.COM>
2025-04-24 21:39:33 +0200

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,10 @@
-
 import os
-import sys
-from pathlib import Path
-
-# Add the parent directory to Python path
-sys.path.append(str(Path(__file__).parent.parent))
 from app import create_app
-
 app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    debug_mode = os.getenv('FLASK_DEBUG', '0').lower() in ['1', 'true']
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
